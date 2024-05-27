54f066ed104d0054039afd5331466bcdde3e75c2
krihor176 <krihor01@easv365.dk>
2024-05-27 16:58:41 +0200

    js for flying dragon on night and day + all image gallery class whole becomes scrollable and adjusted the speed of scrolling

--- script.js
+++ script.js
@@ -44,3 +44,91 @@ window.onclick = function(event) {
     closeModal();
   }
 }
+
+ /* FOR DRAGON TO FLY */
+ window.addEventListener('scroll', function() {
+    var scrollPosition = window.scrollY;
+    var image = document.querySelector('.small-flying-dragon-image');
+    
+    var translateXValue = 0;
+    var translateYValue = 0;
+  
+    if (scrollPosition <= 140) {
+      translateXValue = scrollPosition * 3; 
+      translateYValue = scrollPosition * 3; 
+    } else {
+      translateXValue = 140 * 3 + (scrollPosition - 140) * 1;
+      translateYValue = 140 * 3; 
+    }
+  
+    image.style.transform = 'translate(' + translateXValue + 'px, ' + translateYValue + 'px)';
+    image.style.opacity = 1 - scrollPosition / 2000;
+  });
+
+var images = document.querySelectorAll('.all-images-group img');
+
+var observer = new IntersectionObserver(function(entries) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('loaded');
+    }
+  });
+}, { threshold: 0.2 }); 
+
+images.forEach(function(img) {
+  if (img.complete) {
+    observer.observe(img);
+  } else {
+    img.onload = function() {
+      observer.observe(img);
+    };
+  }
+});
+
+var images = document.querySelectorAll('.all-images-group img');
+
+var observer = new IntersectionObserver(function(entries) {
+  entries.forEach(function(entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('loaded');
+    }
+  });
+}, { threshold: 0.2 }); 
+
+images.forEach(function(img) {
+  if (img.complete) {
+    observer.observe(img);
+  } else {
+    img.onload = function() {
+      observer.observe(img);
+    };
+  }
+});
+
+var imagesGroup = document.querySelector('.all-images-group');
+var isDown = false;
+var startX;
+var scrollLeft;
+
+imagesGroup.addEventListener('mousedown', (e) => {
+  isDown = true;
+  startX = e.pageX - imagesGroup.offsetLeft;
+  scrollLeft = imagesGroup.scrollLeft;
+});
+
+imagesGroup.addEventListener('mouseleave', () => {
+  isDown = false;
+});
+
+imagesGroup.addEventListener('mouseup', () => {
+  isDown = false;
+});
+
+imagesGroup.addEventListener('mousemove', (e) => {
+  if(!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - imagesGroup.offsetLeft;
+  const walk = (x - startX) * 1;
+  imagesGroup.scrollLeft = scrollLeft - walk;
+});
+
